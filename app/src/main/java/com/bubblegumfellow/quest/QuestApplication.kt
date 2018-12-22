8be4ad21007611c89a196f1114e7385208eebd3e
okuzawats <okuzawats@gmail.com>
2018-12-22 14:13:15 +0900

    Timberの導入

--- app/src/main/java/com/bubblegumfellow/quest/QuestApplication.kt
+++ app/src/main/java/com/bubblegumfellow/quest/QuestApplication.kt
@@ -3,6 +3,7 @@ package com.bubblegumfellow.quest
 import android.app.Application
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import timber.log.Timber
 
 class QuestApplication: Application() {
     override fun onCreate() {
@@ -11,6 +12,11 @@ class QuestApplication: Application() {
         // Realm
         Realm.init(this)
         Realm.setDefaultConfiguration(RealmConfiguration.Builder().schemaVersion(0).build())
+
+        // Timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     override fun onTerminate() {
