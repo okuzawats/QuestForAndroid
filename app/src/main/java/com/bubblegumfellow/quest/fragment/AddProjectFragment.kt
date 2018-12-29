67affa6f966e51d4e1bd613d20a12a18a9d0ab1d
okuzawats <okuzawats@gmail.com>
2018-12-29 10:46:36 +0900

    不要なimport文を削除

--- app/src/main/java/com/bubblegumfellow/quest/fragment/AddProjectFragment.kt
+++ app/src/main/java/com/bubblegumfellow/quest/fragment/AddProjectFragment.kt
@@ -6,11 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bubblegumfellow.quest.R
-import com.bubblegumfellow.quest.event.AddProjectEvent
 import kotlinx.android.synthetic.main.fragment_add_project.*
-import org.greenrobot.eventbus.EventBus
 
-class AddProjectFragment: Fragment() {
+class AddProjectFragment : Fragment() {
 
     companion object {
         fun getInstance(): AddProjectFragment {
@@ -27,7 +25,7 @@ class AddProjectFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         addProjectButton.setOnClickListener {
-            val projectName = projectNameEditText.text.toString()
+            val projectName = projectNameEditText.text.toString()
 
             // TODO：ここでRealmに書き込む
 
