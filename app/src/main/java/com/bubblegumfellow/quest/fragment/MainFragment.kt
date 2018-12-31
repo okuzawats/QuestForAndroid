278aab125b3dbbeb8119785fcf08a8d1cc9bcfc7
okuzawats <okuzawats@gmail.com>
2019-01-01 00:04:32 +0900

    MainFragmentの不要なコードを削除

--- app/src/main/java/com/bubblegumfellow/quest/fragment/MainFragment.kt
+++ app/src/main/java/com/bubblegumfellow/quest/fragment/MainFragment.kt
@@ -5,45 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar
 import com.bubblegumfellow.quest.R
-import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeAdapter
-import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeRecyclerView
-import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemDragListener
-import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemSwipeListener
-import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnListScrollListener
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.item_main.view.*
-import java.util.*
 
 class MainFragment: Fragment(), MainViewHolder.ItemClickListener {
-
-    private val onItemSwipeListener = object : OnItemSwipeListener<String> {
-        override fun onItemSwiped(position: Int, direction: OnItemSwipeListener.SwipeDirection, item: String): Boolean {
-            return false
-        }
-    }
-
-    private val onItemDragListener = object : OnItemDragListener<String> {
-        override fun onItemDragged(previousPosition: Int, newPosition: Int, item: String) {
-            // NOP
-        }
-
-        override fun onItemDropped(initialPosition: Int, finalPosition: Int, item: String) {
-            // NOP
-        }
-    }
-
-    private val onListScrollListener = object : OnListScrollListener {
-        override fun onListScrolled(scrollDirection: OnListScrollListener.ScrollDirection, distance: Int) {
-            // NOP
-        }
-    }
 
     companion object {
         fun getInstance(): MainFragment {
